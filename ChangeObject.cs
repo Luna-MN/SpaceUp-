20daf48cc9c1cc94dea9c6e25b1df16077160b82
Matthew Nock <thematt.nock@gmail.com>
2025-03-29 19:38:44 +0000

    more blaster logic

--- ChangeObject.cs
+++ ChangeObject.cs
@@ -11,10 +11,26 @@ public partial class ChangeObject : MeshInstance3D
 	// Called when the node enters the scene tree for the first time.
 	public override void _Ready()
 	{
+		Area3D.Connect("body_entered", new Callable(this, "OnBodyEntered"));
+		Area3D.Connect("body_exited", new Callable(this, "OnBodyExited"));
 	}
 
 	// Called every frame. 'delta' is the elapsed time since the previous frame.
 	public override void _Process(double delta)
 	{
+	}
+	private void OnBodyEntered(Node3D body)
+	{
+		if (body is LocalVeriables player)
+		{
+			// set picked up object here and make it so that it can't have a pickup object
+		}
+	}
+	private void OnBodyExited(Node3D body)
+	{
+		if (body is LocalVeriables player)
+		{
+			// reset picked up object here and make it so that it can have a pickup object
+		}
 	}
 }
